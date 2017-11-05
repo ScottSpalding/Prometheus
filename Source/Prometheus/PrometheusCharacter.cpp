6b342d4f066240bc12319c3e6b7597c651b58b45
Scott Spalding <spaldingscott@gmail.com>
2017-11-05 09:30:49 -0800

    Initial commit

---
+++ Source/Prometheus/PrometheusCharacter.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PrometheusCharacter.h"
+
+#include "Components/PointLightComponent.h"
+#include "Public/TimerManager.h"
+
+// Sets default values
+APrometheusCharacter::APrometheusCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PrometheusLight = CreateDefaultSubobject<UPointLightComponent>(TEXT("PrometheusLight"));
+	PrometheusLight->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void APrometheusCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	ResetLightTimer();
+}
+
+// Called every frame
+void APrometheusCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	//UE_LOG(LogTemp, Warning, TEXT("DamageAmount=%f, DamageToApply=%f"), DamageAmount, DamageToApply)
+	//UE_LOG(LogTemp, Warning, TEXT("Ticking... "))
+	if (PrometheusLight) {
+		float timerMultipler = GetWorldTimerManager().GetTimerRemaining(PrometheusTimerHandle) * MaxLightTime;
+		if (timerMultipler < 0) timerMultipler = 0;
+		float newIntensity = MaxPrometheusLightLevel * timerMultipler;
+		UE_LOG(LogTemp, Warning, TEXT("timerMultipler=%f, intensity=%f"), timerMultipler, newIntensity)
+		PrometheusLight->SetIntensity(newIntensity);
+	}
+	
+}
+
+// Called to bind functionality to input
+void APrometheusCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void APrometheusCharacter::ResetLightTimer() {
+	//UE_LOG(LogTemp, Warning, TEXT("RESET LIGHT TIMER!"))
+	GetWorldTimerManager().SetTimer(PrometheusTimerHandle, this, &APrometheusCharacter::LightsOut, MaxLightTime, false);
+}
+
+void APrometheusCharacter::LightsOut() {
+}
---
+++ Source/Prometheus/PrometheusCharacter.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PrometheusCharacter.generated.h"
+
+UCLASS()
+class PROMETHEUS_API APrometheusCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APrometheusCharacter();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Prometheus")
+	class UPointLightComponent* PrometheusLight;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Prometheus")
+	float MaxPrometheusLightLevel = 2500.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Prometheus")
+	float MaxLightTime = 10.0f;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	void ResetLightTimer();
+
+	void LightsOut();
+
+private:
+
+	FTimerHandle PrometheusTimerHandle;
+	
+};
